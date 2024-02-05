4f3691e3078cf808bc4a9517859b9c50af1dcd42
Gabriel Barbosa <gabrielbarbosaa432@gmail.com>
2024-02-05 18:05:45 -0300

    card-caga

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,56 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule} from '@angular/common/http'
+import { FormsModule, ReactiveFormsModule} from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ListJobsComponent } from './components/list-jobs/list-jobs.component';
+
+
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from  '@angular/material/sidenav'
+import { MatListModule} from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button'
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { HomeComponent } from './pages/home/home.component';
+import { MatTabsModule } from '@angular/material/tabs';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ListJobsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatListModule,
+    MatSidenavModule,
+    MatCardModule,
+    MatButtonModule,
+    HttpClientModule,
+    MatSnackBarModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/interfaces/JobOpenings.ts
@@ -0,0 +1,11 @@
+export interface JobOpenings {
+    id?: number,
+    jobTitle: string,
+    companyName: string,
+    applicationDate: string,
+    applicationWebsite: string,
+    techRecruiter?: string,
+    category: string,
+    applicationUrl: string
+
+}
